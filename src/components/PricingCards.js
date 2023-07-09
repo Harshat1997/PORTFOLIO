277ac081ca916eabdd7187c2ed4b43baebd873d9
harshat <harshalgupta164@gmail.com>
2023-07-09 21:19:56 +0530

    commit

--- src/components/PricingCards.js
+++ src/components/PricingCards.js
@@ -8,34 +8,34 @@ const PricingCards = () => {
         <div className='pricing'>
             <div className='card-container'>
                 <div className='card'>
-                    <h3>- Basic -</h3>
+                    <h3>- CLASS X -</h3>
                     <span className='bar'></span>
-                    <p className='btc'>$ 100</p>
-                    <p>- 3 Days - </p>
-                    <p>- 3 Pages -</p>
-                    <p>- Featured -</p>
-                    <p>- Responsive Designs -</p>
-                    <Link to='/contact' className='btn'>PURCHASE NOW</Link>
+                    {/* <p className='btc'>$ 100</p> */}
+                    <p>- HOLY CROSS SCHOOL - </p>
+                    <p>- ICSE BOARD -</p>
+                    <p>- 81% MARKS-</p>
+                    <p>- PASSED OUT IN 2013.-</p>
+                    <Link to='/contact' className='btn'>NAUTANWA</Link>
                 </div>
                 <div className='card'>
-                    <h3>- Premium -</h3>
-                    <span className='bar'></span>
-                    <p className='btc'>$ 200</p>
-                    <p>- 2 Days - </p>
-                    <p>- 5 Pages -</p>
-                    <p>- Featured -</p>
-                    <p>- Responsive Designs -</p>
-                    <Link to='/contact' className='btn'>PURCHASE NOW</Link>
+                    <h3>- CLASS XII -</h3>
+                     <span className='bar'></span> 
+                    {/* <p className='btc'>$ 200</p> */}
+                    <p>- OXFORD MODEL SCHOOL - </p>
+                    <p>- CBSE BOARD -</p>
+                    <p>- 82% MARKS -</p>
+                    <p>- PASSED OUT IN 2015 -</p>
+                    <Link to='/contact' className='btn'>KANPUR</Link>
                 </div>
                 <div className='card'>
-                    <h3>- Business -</h3>
+                    <h3>- GRADUATION -</h3>
                     <span className='bar'></span>
-                    <p className='btc'>$ 500</p>
-                    <p>- 5 Days - </p>
-                    <p>- 8 Pages -</p>
-                    <p>- Featured -</p>
-                    <p>- Responsive Designs -</p>
-                    <Link to='/contact' className='btn'>PURCHASE NOW</Link>
+                    {/* <p className='btc'>$ 500</p> */}
+                    <p>- GLAGOTIAS COLLEGE - </p>
+                    <p>- AKTU -</p>
+                    <p>- 67% MARKS -</p>
+                    <p>- PASSED OUT IN 2020 -</p>
+                    <Link to='/contact' className='btn'>GREATER NOIDA</Link>
                 </div>
             </div>
         </div>
--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,25 +1,25 @@
-import pro1 from '../assets/project1.jpg'
-import pro2 from '../assets/project2.jpg'
-import pro3 from '../assets/project3.jpg'
+import pro1 from '../assets/youtube.jpg'
+import pro2 from '../assets/ecommerce.jpg'
+import pro3 from '../assets/calculator.jpg'
 
 const ProjectCardData = [
     {
     imgsrc: pro1,
-    title:'E-commerce website',
-    text:'E-commerce (electronic commerce) is the activity of electronically buying or selling of products on online services or over the Internet. E-commerce draws on technologies such as mobile commerce, electronic funds transfer, supply chain management, Internet marketing.',
-    view:'https://en.wikipedia.org/wiki/E-commerce'
+    title:'YOUTUBE APPLICATION',
+    text:'YouTube is a video-sharing site founded by three former PayPal employees Steve Chen, Chad Hurley, and Jawed Karim in February 2005. With millions of YouTubers and billions of videos, YouTube has been a popular website since its release.Accessible worldwide, it was launched on February 14, 2005, by Steve Chen, Chad Hurley, and Jawed Karim ',
+    view:'https://https-github-com-harshat1997-youtube-clone.vercel.app/'
     },
     {
         imgsrc: pro2,
-        title:'Sports website',
-        text:'Football is a family of team sports that involve, to varying degrees, kicking a ball to score a goal. Unqualified, the word football normally means the form of football that is the most popular where the word is used.',
-        view:'https://en.wikipedia.org/wiki/football'
+        title:'ECOMMERCE WEBSITE',
+        text:'Ecommerce examples include B2C websites that sell goods and services to consumers. After a customer makes a purchase, the online retailer delivers the order via shipping, store pickup, or local delivery (in the case of physical products), or digitally (for digital products like PDFs, virtual courses, or online consultations)',
+        view:'https://https-github-com-harshat1997-redux-project.vercel.app/'
         },
         {
             imgsrc: pro3,
-            title:'Entertaunment website',
-            text:'Entertainment is a form of activity that holds the attention and interest of an audience or gives pleasure and delight. It can be an idea or a task, but it is more likely to be one of the activities or events that have developed over thousands of years specifically for the purpose of keeping an audience attention.',
-            view:'https://en.wikipedia.org/wiki/Entertainment'
+            title:'CALCULATOR',
+            text:'An electronic calculator is typically a portable electronic device used to perform calculations, ranging from basic arithmetic to complex mathematics.The first solid-state electronic calculator was created in the early 1960s. Pocket-sized devices became available in the 1970s, especially after the Intel 4004, so it is basically known as calculator',
+            view:'https://caculator-sepia.vercel.app/'
             },
 ];
 
